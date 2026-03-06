9a9eec89185a0a5cbf73ea8d3d6ea3286a09bc55
pc <pc@DESKTOP-2N68CP9>
2026-03-06 14:39:17 +0200

    Studying Heaps

---
+++ Heaps/Heap.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Heap
+{
+
+private:
+    vector<int> heap;
+    int leftChild(int index)
+    {
+        return 2 * index + 1;
+    }
+    int rightChild(int index)
+    {
+        return 2 * index + 2;
+    }
+    int Parent(int index)
+    {
+        return (index - 1) / 2;
+    }
+    void swap(int index1, int index2)
+    {
+        int temp = heap[index1];
+        heap[index1] = heap[index2]; 
+        heap[index2] = temp;
+    }
+
+public:
+    void printHeap()
+    {
+        cout << "\n[";
+        for (size_t i = 0; i < heap.size(); i++)
+        {
+            cout << heap[i];
+            if (i < heap.size() - 1)
+            {
+                cout << " , ";
+            }
+        }
+        cout << "]" << endl;
+    }
+
+    void insert(int value)
+    {
+        heap.push_back(value);
+        int current = heap.size() - 1;
+        while (current > 0 && heap[current] > heap[Parent(current)])
+        {
+            swap(current, Parent(current));
+            current = Parent(current);
+        }
+    }
+};
+
+int main()
+{
+    Heap* heap= new Heap();
+    heap->insert(99);
+    heap->insert(72);
+    heap->insert(61);
+    heap->insert(58);
+    heap->printHeap();
+    heap->insert(100);
+    heap->printHeap();
+    heap->insert(75);
+    heap->printHeap();
+
+
+}
